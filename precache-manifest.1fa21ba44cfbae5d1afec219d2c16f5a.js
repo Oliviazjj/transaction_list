88e6ff6f6ebac6e00fb5b88dfa787fa503bc5b21
Jingjing Zhu <jzhu@pdftron.com>
2019-06-07 14:35:03 -0700

    Updates

---
+++ precache-manifest.1fa21ba44cfbae5d1afec219d2c16f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b955fa477791e091ab0db9e35104d2",
+    "url": "/transaction_list/index.html"
+  },
+  {
+    "revision": "b5434253c53dde26f8af",
+    "url": "/transaction_list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/transaction_list/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b5434253c53dde26f8af",
+    "url": "/transaction_list/static/js/main.3aa38b34.chunk.js"
+  },
+  {
+    "revision": "ce85b01dc780fd440dde",
+    "url": "/transaction_list/static/js/runtime~main.2103d1eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/transaction_list/static/media/logo.5d5d9eef.svg"
+  }
+]);
