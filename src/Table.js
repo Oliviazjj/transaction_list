fab6dbc81a8e05d841cba72b2f6bab26623be50e
Jingjing Zhu <jzhu@pdftron.com>
2019-08-28 23:35:20 -0700

    update table

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import axios from "axios";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import BootstrapTable, {TableHeaderColumn} from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
@@ -8,7 +11,7 @@ import filterFactory, { textFilter, selectFilter, numberFilter, Comparator, mult
 
 
 
-const products = [{'id': 0, 'name': 'name1', 'price': 0.5, 'quality': 0}, {'id': 1, 'name': 'name2', 'price': 5.5, 'quality': 1}, {'id': 2, 'name': 'name3', 'price': 2.5, 'quality': 0}];
+const transactionItems = [{'id': 0, 'name': 'name1', 'price': 0.5, 'quality': 0}, {'id': 1, 'name': 'name2', 'price': 5.5, 'quality': 1}, {'id': 2, 'name': 'name3', 'price': 2.5, 'quality': 0}];
 
 const quality_selectOptions = {
   0: 'good',
@@ -123,7 +126,84 @@ function priceFormatter(column, colIndex, { sortElement, filterElement }) {
   );
 }
 
+const MySearch = (props) => {
+  let input;
+  const handleClick = () => {
+    props.onSearch(input.value);
+  };
+  return (
+    <div class="row search_field">
+      <div class="centered-label">
+        <FontAwesomeIcon icon={faSearch} />
+      </div>
+      <input
+        className="search_bar"
+        placeholder="搜索"
+        style={ { backgroundColor: 'white' } }
+        ref={ n => input = n }
+        type="text"
+        onChange={ handleClick }
+      />
+    </div>
+  );
+};
+
+const MyExportCSV = (props) => {
+  const handleClick = () => {
+    props.onExport();
+  };
+  return (
+    <div>
+      <button className="btn btn-success" onClick={ handleClick }>下载CSV</button>
+    </div>
+  );
+};
+
+
 class Table extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      transactionList: []
+    };
+  }
+  componentDidMount() {
+    this.refreshList();
+  }
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/transactions/")
+      .then(res => this.setState({ transactionList: res.data }))
+      .catch(err => console.log(err));
+  };
+  displayCompleted = status => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+
+  handleSubmit = item => {
+    this.toggle();
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/transactions/${item.id}/`, item)
+        .then(res => this.refreshList());
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/transactions/", item)
+      .then(res => this.refreshList());
+  };
+  handleDelete = item => {
+    axios
+      .delete(`http://localhost:8000/api/transactions/${item.id}`)
+      .then(res => this.refreshList());
+  };
+
   render() {
     return (
 		<ToolkitProvider
@@ -132,8 +212,10 @@ class Table extends React.Component {
 			striped
 			hover
 			condensed
-			data={ products } 
+			data={ this.state.transactionList } 
 			columns={ columns }
+      insertRow ={true} 
+      deleteRow={ true }
 			columnToggle
 			loading={ true }  //only loading is true, react-bootstrap-table will render overlay
 			overlay={ overlayFactory() }
@@ -144,22 +226,33 @@ class Table extends React.Component {
 			pagination={ paginationFactory() }
 			selectRow={ { mode: 'checkbox', clickToSelect: true } }
 			tabIndexCell
-//			expandRow={ expandRow }
 			defaultSorted={ defaultSorted } 
 			search
 		>
 		{
 			props => (
-				<div>
-				<h3>Toggle columns below:</h3>
-				<ToggleList { ...props.columnToggleProps } />
-				<SearchBar { ...props.searchProps } />
-				<ClearSearchButton { ...props.searchProps } />
-				<ExportCSVButton { ...props.csvProps }>Export CSV!!</ExportCSVButton>
-				<hr />
-		  		<BootstrapTable 
-					{ ...props.baseProps }
-				/>
+				<div class="content">
+          <div class="row">
+            <h1 className="text-gray text-uppercase text-center my-4">交易列表</h1>
+            <div className="float-right">
+                <MyExportCSV { ...props.csvProps } />
+            </div>
+          </div>
+          <div class="transaction_list">
+            
+            <div>
+              <MySearch { ...props.searchProps } />
+            </div>
+            <div class="row">
+              <h3 class="col-md-3">选择显示/隐藏:</h3>
+              <ToggleList { ...props.columnToggleProps } />
+            </div>
+    				
+    				<hr />
+    		  		<BootstrapTable 
+    					{ ...props.baseProps }
+    				/>
+          </div>
 				</div>
 			)
 		}
