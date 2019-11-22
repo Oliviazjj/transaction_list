c157714115f431797002447351620df723318db9
Jingjing Zhu <jzhu@pdftron.com>
2019-11-21 20:16:09 -0800

    add unit test to model

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -1,35 +1,22 @@
 from django.db import models
+from transaction.constants import *
 
 # Create your models here.
-
-QUALITY_CHOICES = {
-  (0, 1),
-  (1, 2),
-  (2, 3),
-  (3, 4),
-  (4, 5),
-};
-
-SPECIFICATION_CHOICES = (
-        (0, '平方米'),
-        (1, '立方米'),
-        (2, '未知'),
-    )
 
 class ListItem(models.Model):
 	item_id = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
 	brand = models.CharField(max_length=200)
-	unit_price = models.DecimalField(max_digits=20, decimal_places=4)
-	total_price = models.DecimalField(max_digits=20, decimal_places=4)
-	specification = models.CharField(max_length=10, choices=SPECIFICATION_CHOICES, default='UNKNOWN')
-	quality = models.BigIntegerField()
-	vendor = models.CharField(max_length=200)
-	agent = models.CharField(max_length=200)
+	unit_price = models.DecimalField(..., max_digits=5, decimal_places=2)
+	total_price = models.DecimalField(..., max_digits=5, decimal_places=2)
+	specification = models.CharField(max_length=200)
+	quality = models.CharField(max_length=200)
+	vendor = models.CharField(null=True, max_length=200)
+	agent = models.CharField(null=True, max_length=200)
 	receipt_bool = models.BooleanField()
-	created_date = models.DateField()
+	created_date = models.DateField(null=True, blank=True)
 
 
 	def __str__(self):
 		"""A string representation of the model."""
-		return 'Item id:{}, name: {}, brand: {}, unit_price: {}, total_price: {}, specification: {}, quality: {}, vendor: {}, agent: {}, receipt_bool:{}, created_date: {}'.format(self.id, self.name, self.brand, self.unit_price, self.total_price, self.specification, self.quality, self.vendor, self.agent, self.receipt_bool, self.created_date)
+		return 'Item id:{}, name: {}, brand: {}, unit_price: {}, total_price: {}, specification: {}, quality: {}, vendor: {}, agent: {}, receipt_bool:{}, created_date: {}'.format(self.item_id, self.name, self.brand, self.unit_price, self.total_price, self.specification, self.quality, self.vendor, self.agent, self.receipt_bool, self.created_date)
--- backend/transaction/tests.py
+++ backend/transaction/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from transaction.models import ListItem as ListItemModel
+from transaction.constants import *
+import datetime
 
-# Create your tests here.
+# listItem tests
+
+
+class ListItem(TestCase):
+        @classmethod
+        def setUpTestData(cls):
+                # Set up non-modified objects used by all test methods
+
+                item1 = ListItemModel.objects.create(item_id='1234', name='name1', brand='brand1', unit_price=12.0, total_price=120.0, specification='立方米', quality=10, vendor='vendor1', agent='agent1', receipt_bool='True', created_date=datetime.date.today())
+        
+        def test_list_item_setup(self):
+                print("Method: test_list_item_setup.")
+                item1 = ListItemModel.objects.get(item_id='1234')
+                self.assertEqual(str(item1), "Item id:1234, name: name1, brand: brand1, unit_price: 12.00, total_price: 120.00, specification: 立方米, quality: 10, vendor: vendor1, agent: agent1, receipt_bool:True, created_date: 2019-11-22")
