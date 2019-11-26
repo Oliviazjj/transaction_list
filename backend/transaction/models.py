6ce26af755edb7d28ea1edd4d67833fd4644adb0
Jingjing Zhu <jzhu@pdftron.com>
2019-11-25 22:22:49 -0800

    debug test and models

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -3,12 +3,14 @@ from transaction.constants import *
 
 # Create your models here.
 
+
 class ListItem(models.Model):
 	item_id = models.CharField(max_length=200)
 	name = models.CharField(null=True, max_length=200)
 	brand = models.CharField(null=True, max_length=200)
-	unit_price = models.DecimalField(..., max_digits=5, decimal_places=2, null=True)
-	total_price = models.DecimalField(..., max_digits=5, decimal_places=2, null=True)
+	unit_price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+	total_price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+	quantity = models.IntegerField(null=True)
 	specification = models.CharField(max_length=200, null=True)
 	quality = models.CharField(max_length=200, null=True)
 	vendor = models.CharField(null=True, max_length=200)
@@ -19,4 +21,4 @@ class ListItem(models.Model):
 
 	def __str__(self):
 		"""A string representation of the model."""
-		return 'Item id:{}, name: {}, brand: {}, unit_price: {}, total_price: {}, specification: {}, quality: {}, vendor: {}, agent: {}, receipt_bool:{}, created_date: {}'.format(self.item_id, self.name, self.brand, self.unit_price, self.total_price, self.specification, self.quality, self.vendor, self.agent, self.receipt_bool, self.created_date)
+		return 'Item id:{}, name: {}, brand: {}, unit_price: {}, total_price: {}, quantity: {}, specification: {}, quality: {}, vendor: {}, agent: {}, receipt_bool:{}, created_date: {}'.format(self.item_id, self.name, self.brand, self.unit_price, self.total_price, self.quantity, self.specification, self.quality, self.vendor, self.agent, self.receipt_bool, self.created_date)
--- backend/transaction/tests.py
+++ backend/transaction/tests.py
@@ -11,9 +11,9 @@ class ListItem(TestCase):
         def setUpTestData(cls):
                 # Set up non-modified objects used by all test methods
 
-                item1 = ListItemModel.objects.create(item_id='1234', name='name1', brand='brand1', unit_price=12.0, total_price=120.0, specification='立方米', quality=10, vendor='vendor1', agent='agent1', receipt_bool='True', created_date=datetime.date.today())
+                item1 = ListItemModel.objects.create(item_id='1234', name='name1', brand='brand1', unit_price=12.0, total_price=120.0, quantity=10, specification='立方米', quality=3, vendor='vendor1', agent='agent1', receipt_bool='True', created_date=datetime.date.today())
         
         def test_list_item_setup(self):
                 print("Method: test_list_item_setup.")
                 item1 = ListItemModel.objects.get(item_id='1234')
-                self.assertEqual(str(item1), "Item id:1234, name: name1, brand: brand1, unit_price: 12.00, total_price: 120.00, specification: 立方米, quality: 10, vendor: vendor1, agent: agent1, receipt_bool:True, created_date: 2019-11-22")
+                self.assertEqual(str(item1), "Item id:1234, name: name1, brand: brand1, unit_price: 12.00, total_price: 120.00, quantity: 10, specification: 立方米, quality: 3, vendor: vendor1, agent: agent1, receipt_bool:True, created_date: 2019-11-26")
