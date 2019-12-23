172a2a9f22f3f805da6171033099f43f5d701e08
Jingjing Zhu <jzhu@pdftron.com>
2019-12-23 12:39:09 -0800

    add filters into columns

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from 'styled-components'
-import { useTable, useResizeColumns, useFlexLayout } from 'react-table'
+import { useTable, useResizeColumns, useFlexLayout, useSortBy, useFilters, useGlobalFilter } from 'react-table'
+// A great library for fuzzy filtering/sorting items
+import matchSorter from 'match-sorter'
 
 const Styles = styled.div`
   padding: 1rem;
@@ -70,7 +72,201 @@ const Styles = styled.div`
   }
 `
 
+// Define a default UI for filtering
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={globalFilter || ''}
+        onChange={e => {
+          setGlobalFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={e => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+// This is a custom filter UI that uses a
+// slider to set the filter value between a column's
+// min and max values
+function SliderColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the min and max
+  // using the preFilteredRows
+
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    preFilteredRows.forEach(row => {
+      min = Math.min(row.values[id], min)
+      max = Math.max(row.values[id], max)
+    })
+    return [min, max]
+  }, [id, preFilteredRows])
+
+  return (
+    <>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={filterValue || min}
+        onChange={e => {
+          setFilter(parseInt(e.target.value, 10))
+        }}
+      />
+      <button onClick={() => setFilter(undefined)}>Off</button>
+    </>
+  )
+}
+
+// This is a custom UI for our 'between' or number range
+// filter. It uses two number boxes and filters rows to
+// ones that have values between the two
+function NumberRangeColumnFilter({
+  column: { filterValue = [], preFilteredRows, setFilter, id },
+}) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    preFilteredRows.forEach(row => {
+      min = Math.min(row.values[id], min)
+      max = Math.max(row.values[id], max)
+    })
+    return [min, max]
+  }, [id, preFilteredRows])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <input
+        value={filterValue[0] || ''}
+        type="number"
+        onChange={e => {
+          const val = e.target.value
+          setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
+        }}
+        placeholder={`Min (${min})`}
+        style={{
+          width: '70px',
+          marginRight: '0.5rem',
+        }}
+      />
+      to
+      <input
+        value={filterValue[1] || ''}
+        type="number"
+        onChange={e => {
+          const val = e.target.value
+          setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
+        }}
+        placeholder={`Max (${max})`}
+        style={{
+          width: '70px',
+          marginLeft: '0.5rem',
+        }}
+      />
+    </div>
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+
+
+
 function ReactTable({ columns, data }) {
+
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
 
   const defaultColumn = React.useMemo(
     () => ({
@@ -78,6 +274,7 @@ function ReactTable({ columns, data }) {
       minWidth: 30, // minWidth is only used as a limit for resizing
       width: 150, // width is used for both the flex-basis and flex-grow
       maxWidth: 200, // maxWidth is only used as a limit for resizing
+      Filter: DefaultColumnFilter, //default filter UI
     }),
     []
   )
@@ -89,16 +286,24 @@ function ReactTable({ columns, data }) {
     headerGroups,
     rows,
     prepareRow,
+    state,
     totalColumnsWidth,
+    flatColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
 
   } = useTable(
     {
       columns,
       data,
       defaultColumn,
+      filterTypes,
     },
     useResizeColumns,
-    useFlexLayout
+    useFlexLayout,
+    useFilters, 
+    useGlobalFilter,
+    useSortBy,
   )
 
   return (
@@ -107,17 +312,31 @@ function ReactTable({ columns, data }) {
         {headerGroups.map(headerGroup => (
           <div {...headerGroup.getHeaderGroupProps()} className="tr">
             {headerGroup.headers.map(column => (
-              <div {...column.getHeaderProps()} className="th">
+              <div {...column.getHeaderProps(column.getSortByToggleProps())} className="th">
                 {column.render('Header')}
+                <span>
++                 {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
++               </span>
                 {/* Use column.getResizerProps to hook up the events correctly */}
                 <div
                   {...column.getResizerProps()}
                   className={`resizer ${column.isResizing ? 'isResizing' : ''}`}
                 />
+                {/* Render the columns filter UI */}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
               </div>
             ))}
           </div>
         ))}
+        <div className="tr">
+          <div colSpan={flatColumns.length} style={{ textAlign: 'left',}} className="th">
+            <GlobalFilter
+              preGlobalFilteredRows={preGlobalFilteredRows}
+              globalFilter={state.globalFilter}
+              setGlobalFilter={setGlobalFilter}
+            />
+          </div>
+        </div>
       </div>
 
       <div {...getTableBodyProps()} className="tbody">
@@ -140,6 +359,21 @@ function ReactTable({ columns, data }) {
   )
 }
 
+// Define a custom filter filter function!
+function filterGreaterThan(rows, id, filterValue) {
+  return rows.filter(row => {
+    const rowValue = row.values[id]
+    return rowValue >= filterValue
+  })
+}
+
+// This is an autoRemove method on the filter function that
+// when given the new filter value and returns true, the filter
+// will be automatically removed. Normally this is just an undefined
+// check, but here, we want to remove the filter if it's not a number
+
+filterGreaterThan.autoRemove = val => typeof val !== 'number'
+
 
 function Table({data}) {
 
@@ -148,57 +382,81 @@ function Table({data}) {
           {
             Header: '序号',
             accessor: 'id',
+            sortType: 'basic'
           },
           {
             Header: '产品ID',
             accessor: 'item_id',
+            sortType: 'basic',
+            filter: 'fuzzyText',
+          },
+          {
+            Header: '名称',
+            accessor: 'name',
+            sortType: 'basic'
           },
           {
             Header: '品牌',
             accessor: 'brand',
-          },
-          {
-            Header: '品牌',
-            accessor: 'brand',
+            sortType: 'basic'
           },{
             Header: '单价',
             accessor: 'unit_price',
+            sortType: 'basic',
+            Filter: SliderColumnFilter,
+            filter: filterGreaterThan,
           },{
             Header: '总价',
             accessor: 'total_price',
+            sortType: 'basic',
+            Filter: SliderColumnFilter,
+            filter: filterGreaterThan,
           },{
             Header: '数量',
             accessor: 'quantity',
+            sortType: 'basic',
+            Filter: SliderColumnFilter,
+            filter: filterGreaterThan,
           },{
             Header: '规格',
             accessor: 'specification',
+            Filter: SelectColumnFilter,
+            filter: 'includes',
           },{
             Header: '质量',
             accessor: 'quality',
+            Filter: SliderColumnFilter,
+            filter: filterGreaterThan,
           },{
             Header: '供应商',
             accessor: 'vendor',
+            sortType: 'basic'
           },{
             Header: '经手人',
             accessor: 'agent',
+            sortType: 'basic'
           },{
             Header: '有无发票',
             accessor: 'receipt_bool',
+            Filter: SelectColumnFilter,
+            filter: 'includes',
           },{
             Header: '创建时间',
             accessor: 'created_date',
+            sortType: 'date'
           },{
             Header: '更新时间',
             accessor: 'updated_date',
+            sortBy: 'date'
           },{
             Header: 'Edit',
             accessor: '[row identifier to be passed to button]',
-            Cell: ({value}) => (<button>Edit</button>)
+            Cell: (row) => (<button onClick={(row) => alert("edit data is "+row.id)} >Edit</button>)
           },
           {
             Header: 'Delete',
             accessor: '[row identifier to be passed to button]',
-            Cell: ({value}) => (<button>Delete</button>)
+            Cell: (rows) => (<button>Delete</button>)
           },
         ],
       []
